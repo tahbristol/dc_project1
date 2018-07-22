89acd90a008df0fe99d46d56e5f560af0791d29e
tahbristol <tahbristol@gmail.com>
2018-07-21 23:24:06 -0400

    initial commit

---
+++ app/assets/javascripts/components/_landing.js.jsx
@@ -0,0 +1,14 @@
+class Page extends React.Component {
+	
+	componentDidMount() {
+	}
+	
+	render(){
+		return(
+			<div>
+				<p>Landing Page</p>
+				<button>Sign Up</button>
+			</div>
+		)
+	}
+}
