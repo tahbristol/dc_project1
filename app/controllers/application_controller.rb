96aaa66333f5febf4908bd2c86bc2594251b6273
tahbristol <tahbristol@gmail.com>
2018-08-06 22:28:26 -0400

    delete dummy code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :null_session
-	
-	def user_id_lookup(username)
-		userInfo = Twitter.getUserInfo(username)
-		userId = userInfo.id
-	end
-	
-	
-	
 end
