f6e6de416421830ae7565083451705742c4b4b1d
TylerB <github email address>
2018-09-06 19:10:03 -0400

    added spec folders/files

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe "User" do
+
+    describe "relationships"
+
+    describe "attributes"
+
+    describe "methods"
+    
+  end
+end
