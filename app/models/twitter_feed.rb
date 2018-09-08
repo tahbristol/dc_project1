51025e665e4a311727124b40587d4a814abb7e8e
tahbristol <tahbristol@gmail.com>
2018-09-08 14:53:38 -0400

    twitter feed spec and model refactor

--- app/models/twitter_feed.rb
+++ app/models/twitter_feed.rb
@@ -1,38 +1,35 @@
 class TwitterFeed < ApplicationRecord
 	
-	attr_accessor :current_user
+	attr_accessor :current_user, :client, :followed_account
 
 	def initialize(user)
 		@current_user = user
+		@client = initTwitterApi
 	end
 
 	def initTwitterApi
-		client = Twitter::REST::Client.new do |config|
+		Twitter::REST::Client.new do |config|
   		config.consumer_key = ENV['CONSUMER_KEY']
   		config.consumer_secret = ENV['CONSUMER_SECRET']
   		config.access_token = ENV['ACCESS_TOKEN']
   		config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 		end
-		client
 	end
 	
-	def get_followed_account(username)
-		client = self.initTwitterApi
-		followed_account_info = client.user(username)
+	def get_and_save_followed_account(twitterHandle)
+		followed_account_info = @client.user(twitterHandle)
 		platform = Platform.find_or_create_by(name:"Twitter",api_base_url:"https://api.twitter.com/1.1/", user_id: @current_user.id)
-		new_followed_account = FollowedAccount.find_or_create_by(
+		@followed_account = FollowedAccount.find_or_create_by(
 			account_id: followed_account_info.id,
-			account_name: username,
+			account_name: twitterHandle,
 			platform_id: platform.id
 		)
-		platform.followed_accounts << new_followed_account
+		platform.followed_accounts << @followed_account
 		platform.save
-		get_tweets(client, new_followed_account)
-		"complete"
 	end
 	
-	def get_tweets(client, followed_account)
-		tweets = client.user_timeline(followed_account.account_id)
+	def get_tweets
+		tweets = @client.user_timeline(@followed_account.account_id)
 		tweets.each do |tweet|
 			Post.find_or_create_by(
 				content: tweet.text,
@@ -40,7 +37,7 @@ class TwitterFeed < ApplicationRecord
 				author: tweet.user.name,
 				social_media_platform: "Twitter",
 				marked_as_read: false,
-				followed_account_id: followed_account.id
+				followed_account_id: @followed_account.id
 			)
 		end 
 	end
--- spec/models/twitter_feed_spec.rb
+++ spec/models/twitter_feed_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe TwitterFeed, type: :model do
-
+	
+	describe "TwitterFeed" do
+		it "initializes with client object" do
+			twitter_feed = TwitterFeed.new 
+			
+			expect(twitter_feed.client).to be_a Twitter::REST::Client
+		end
+		
+	end
+	
+	describe "#get_and_save_followed_account" do
+		before {@user = User.create(email: "user@email.com", password: "password")}
+		it "uses twitter handle to get twitter account info" do
+			twitter_feed = TwitterFeed.new(@user)
+			twitter_feed.get_and_save_followed_account("tahbristol")
+			
+			expect(twitter_feed.followed_account).not_to eq(nil)
+		end
+	end
 end
