8d3b8df61f705064e8a408a8ed1b4022f3b38e5c
tahbristol <tahbristol@gmail.com>
2018-10-13 19:49:57 -0400

    removed unused code

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,17 +2,4 @@ class Post < ApplicationRecord
 	validates :author, presence: true
 	belongs_to :followed_account
 	
-	
-	
-	def self.get_for_user(user) #not used anywhere, just for testing
-		@posts = []
-		platform = Platform.where(user_id: user.id, name: "Twitter").first
-		followed_accounts = platform.followed_accounts
-		followed_accounts.each do |fa| #n + 1
-			fa.posts.each do |post|
-				@posts << post
-			end
-		end 
-		@posts
-	end
 end
