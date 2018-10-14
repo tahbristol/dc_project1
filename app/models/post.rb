bedcc831c20a603b727059dce831dc75d9e74871
tahbristol <tahbristol@gmail.com>
2018-10-13 23:24:41 -0400

    scope for unread and ordered posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
 	validates :author, presence: true
 	belongs_to :followed_account
 	
+	scope :unread, -> {
+		where(marked_as_read: false)
+	}
+	
+	scope :ordered_desc, -> {
+		order('timestamp DESC')
+	}
+
 end
