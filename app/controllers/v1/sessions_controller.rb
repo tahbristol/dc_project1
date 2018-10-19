7447ba797424df036ec918f5f23e6620c05ef3fa
tahbristol <tahbristol@gmail.com>
2018-10-18 20:14:36 -0400

    removed &

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -2,7 +2,6 @@ class V1::SessionsController < ApplicationController
 	
 	def create
 		@user = User.find_by(email: params[:email])
-		
 		if @user&.valid_password?(params[:password])
 			render json: @user.as_json(only: [:email, :authentication_token, :hasFollowedAccounts]), status: :created
 		else
@@ -11,7 +10,7 @@ class V1::SessionsController < ApplicationController
 	end
 	
 	def destroy
-		current_user&.authentication_token = nil
+		current_user.authentication_token = nil
 		if current_user.save
 			head(:ok)
 		else
