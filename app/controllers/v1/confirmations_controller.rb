f61c911e3e78fa72e0ac6bd9e8f6f242634e4240
tahbristol <tahbristol@gmail.com>
2018-10-21 13:27:13 -0400

    prevent re-confirming

--- app/controllers/v1/confirmations_controller.rb
+++ app/controllers/v1/confirmations_controller.rb
@@ -2,7 +2,7 @@ class V1::ConfirmationsController < ApplicationController
 	def new 
 		if params[:token].present?
 			@user = User.find_by(confirmation_token: params[:token])
-			@user.update_attributes(confirmed_at: DateTime.now)
+			@user.update_attributes(confirmed_at: DateTime.now) unless @user.confirmed?
 		end
 		redirect_to "http://localhost:3000/login"
 	end
