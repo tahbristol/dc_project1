e892f6a5cdc7ee3c3c5317a2bcd93fc83b08917b
tahbristol <tahbristol@gmail.com>
2018-10-30 21:34:37 -0400

    added sendgrid config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,8 +47,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-	config.action_mailer.delivery_method = :smtp
-	config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+	#config.action_mailer.delivery_method = :smtp
+	#config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'localhost',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
