26977bd3304a8619510f0d9ca013a0f836fd6c25
tahbristol <tahbristol@gmail.com>
2018-10-30 22:12:22 -0400

    log in with token

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,8 +1,16 @@
 class V1::SessionsController < ApplicationController
 	
 	def create
-		@user = User.find_by(email: params[:email])
-		if @user.confirmed? && @user&.valid_password?(params[:password])
+		start_session = false
+		if params[:token].present?
+			@user = User.find_by(confirmation_token: params[:token])
+			start_session = new_with_token
+		else
+			@user = User.find_by(email: params[:email])
+			start_session = new_with_password
+		end
+		
+		if start_session
 			render json: @user.as_json(only: [:email, :authentication_token, :hasFollowedAccounts]), status: :created
 		else
 			head(:unauthorized)
@@ -17,5 +25,18 @@ class V1::SessionsController < ApplicationController
 			head(:unauthorized)
 		end
 	end
+	
+	private
+		def new_with_token
+			if @user&.confirmed?
+				true
+			end
+		end
+		
+		def new_with_password
+			if @user.confirmed? && @user&.valid_password?(params[:password])
+				true
+			end
+		end
 	
 end
